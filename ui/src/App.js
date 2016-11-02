aba8acb15a719cb7a6d306bb81b91695388f3bd5
Jonas Faber <dev.jonas.faber@gmail.com>
2016-11-02 16:55:40 +0100

    Change styling

--- ui/src/App.js
+++ ui/src/App.js
@@ -21,23 +21,19 @@ const add = (data) => () => {
     codeA: [''],
   })
 }
-const changeActive = (data) => () => {
-  data.active = 10;
+const remove = (data) => () => {
+  data.data.codes.snippet.splice(data.active, 1)
 }
+
 const Note = observer(({snippet, remove, disabled = false}) => (
   <div style={{}} className="note">
     <input style={{width: '40%'}} value={snippet.$.name} onChange={({target})=>snippet.$.name=target.value} />
-    <button onClick={remove} disabled={disabled}>
-      Delete
-    </button>
-
     <Code value={snippet.codeA[0]} onChange={(newCode)=>snippet.codeA[0]=newCode}/>
 
   </div>
 ));
 //
 const List = observer(({data}) => {
-  const remove = (index) => () => data.codes.snippet.splice(index, 1)
   const select = (index) => () => {
     data.active = index
   }
@@ -46,13 +42,14 @@ const List = observer(({data}) => {
     return (
       <div className="wrapper">
       <div className="codeView">
-        {data.data.codes.snippet[data.active] && <Note snippet={data.data.codes.snippet[data.active]} remove={remove(data.active)} />}
+        {data.data.codes.snippet[data.active] && <Note snippet={data.data.codes.snippet[data.active]} />}
       </div>
       <div className="codeList">
         <ul>{data.data.codes.snippet.map((snippet, index)=>
           <li
             key={index}
             onClick={select(index)}
+            className={index === data.active ?'active':''}
           >
             {snippet.$.name}
           </li>
@@ -68,16 +65,18 @@ const List = observer(({data}) => {
 const App = ({data, file}) => (
   <DocumentTitle title={`${file.pathInfo.name?file.pathInfo.name:'Untitled'}${file.pathInfo.ext?file.pathInfo.ext:''} - XML-Editor - ${file.pathInfo.dir?file.pathInfo.dir:'Not Saved'}`}>
     <div className="App">
+    {process.env.NODE_ENV === "development"?<Debuger />:null}
       <div className="App-header">
           <button onClick={load(file)}>Open...</button>
           <button onClick={save(file, data)} disabled={file.pathInfo.name === ''}>Save</button>
           <button onClick={saveAs(file, data)}>Save As</button>
           <button onClick={add(data)}>Add Snippet</button>
-          <button onClick={changeActive(data)}>changeActive</button>
-
+          <button onClick={remove(data)} disabled={data.data.codes.snippet.length <= 1}>
+            Delete
+          </button>
       </div>
       {file.loading?<div className="loading">loading...</div>:<List data={data} />}
-      <Debuger />
+
     </div>
   </DocumentTitle>
 );
--- ui/src/Code.js
+++ ui/src/Code.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/theme/neo.css';
 
 import Codemirror from 'react-codemirror';
 import './Code.css';
 
 const options = {
+	mode: 'text/x-c++src',
 	lineNumbers: true,
 	readOnly: false,
+	theme: 'neo',
 }
 const Code = ({ onChange, value }) => <Codemirror value={value} onChange={onChange} options={options}/>
 export default Code;
